23bdedc3de70027cc1c3324f9a87cc2ce6b052c8
Agustín Martina <amartina@unc.edu.ar>
2021-04-06 09:02:42 -0300

    config files for project

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+import os
+
+verbose = False
+debug = False
+
+base_url = 'https://bdhm.ohmc.ar/api/'
+
+token = os.getenv('TOKEN')
+usr = os.getenv('USER')
+secret = os.getenv('SECRET')
+
+headers = {'Authorization': 'Token ' + token}
+
+
+gfs_info = {'name': 'GFS', 'param': 'Unique', 'run': '00:00', 'version': '1.0',
+            'grid_resolution': '[0.25º]x[0.25º]', 'global_model': 'GFS'}
