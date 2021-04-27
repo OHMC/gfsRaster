8b6cf3e4cad4a4aa300db8d6d3b4bd4f10d7047e
Agustin Martina <amartina@unc.edu.ar>
2021-04-26 23:28:23 -0300

    fixed dictionary names

--- config/constants.py
+++ config/constants.py
@@ -11,8 +11,8 @@ secret = os.getenv('SECRET')
 
 variables = {
         'T2P': 'gfs_mean_region_temperature',
-        'wdir10': 'gfs_mean_region_wdir',
-        'wspd10': 'gfs_mean_region_wspd'
+        'wdir': 'gfs_mean_region_wdir',
+        'wspd': 'gfs_mean_region_wspd'
     }
 
 gfs_info = {'name': 'GFS', 'param': 'Unique', 'run': '00:00', 'version': '1.0',
