853ffd9f0b24d5576103982a86ee4b976f26787b
Agustin Martina <amartina@unc.edu.ar>
2021-05-21 15:47:32 -0300

    new script for wrf GFS

--- config/constants.py
+++ config/constants.py
@@ -17,3 +17,7 @@ variables = {
 
 gfs_info = {'name': 'GFS', 'param': 'Unique', 'run': '00:00', 'version': '1.0',
             'grid_resolution': '[0.25º]x[0.25º]', 'global_model': 'GFS'}
+
+EXTENT = [-64.0, -43.8, -60.0, -42.8]
+KM_PER_DEGREE = 111.32
+RESOLUTION = 1
